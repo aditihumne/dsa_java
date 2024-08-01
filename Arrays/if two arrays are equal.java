36dd53cb2a3427246761773e203a678ffcb8098a
aditihumne <147162686+aditihumne@users.noreply.github.com>
2024-08-01 23:13:14 +0530

    Add files via upload

---
+++ Arrays/if two arrays are equal.java
@@ -0,0 +1,16 @@
+
+import java.util.Arrays ; 
+public class Main
+{
+	public static void main(String[] args) {
+		int []a={23,45,67,34};
+		int []b= {23,45,67,34,45};
+		boolean result= Arrays.equals(a,b);
+		if (result==true){
+		    System.out.println("the arrays are equal");
+		}else {
+		    System.out.println("the arrays are not equal");
+		}
+	}
+	
+}
