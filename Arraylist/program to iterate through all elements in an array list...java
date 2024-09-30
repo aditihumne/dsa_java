4c5098c35e3dfcdedf301eb05561afe4490dc46a
aditihumne <147162686+aditihumne@users.noreply.github.com>
2024-09-30 12:13:58 +0530

    Add files via upload

---
+++ Arraylist/program to iterate through all elements in an array list...java
@@ -0,0 +1,29 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+import java.util.ArrayList;
+import java.util.List ; 
+public class Main
+{
+	public static void main(String[] args) {
+	    List<String> addition = new ArrayList<>();
+	    addition.add("orange");
+	    addition.add("black");
+	    addition.add("green");
+	    addition.add("white");
+	    addition.add("black");
+	    for(int i=0 ; i<addition.size();i++){
+	        System.out.println(addition.get(i));
+	    }
+	    
+	    
+	   
+	       
+	    
+	
+	}
+}
