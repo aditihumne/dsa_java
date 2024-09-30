e029a4fc402cbf2fb1cbc7160b0db8e77478637a
aditihumne <147162686+aditihumne@users.noreply.github.com>
2024-09-30 12:53:16 +0530

    Add files via upload

---
+++ Arraylist/Java program to sort a given array list..java
@@ -0,0 +1,26 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+import java.util.ArrayList;
+import java.util.List ; 
+import java.util.*; 
+public class Main
+{
+	public static void main(String[] args) {
+	    List<String> addition = new ArrayList<>();
+	    addition.add("orange");
+	    addition.add("pink");
+	    addition.add("green");
+	    addition.add("white");
+	    addition.add("black");
+	    
+	   System.out.println(addition);
+	   Collections.sort(addition);
+	   System.out.println(addition);
+	        
+	}
+}
