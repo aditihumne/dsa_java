6cb7a308665cfde9e7cefa8a36a4cec3c0419197
aditihumne <147162686+aditihumne@users.noreply.github.com>
2025-06-30 15:48:04 +0530

    palindrome.java

--- Strings/palindrome.java
+++ Strings/palindrome.java
@@ -7,31 +7,22 @@ Write your code in this editor and press "Run" button to execute it.
 *******************************************************************************/
 public class Main {
     public static void main(String[] args) {
-        String str = "abba";
-        str = str.toLowerCase(); // Convert to lowercase to make it case-insensitive
-        
-        // Check if the string is a palindrome
-        if (ispalindrome(str)) {
-            System.out.println("yes");
-        } else {
-            System.out.println("no");
-        }
-    }
-    
-    // Method to check if a string is a palindrome
-    public static boolean ispalindrome(String str) {
-        int i = 0; 
-        int j = str.length() - 1;
-        
-        // Loop to compare characters from start and end
-        while (i < j) {
-            if (str.charAt(i) != str.charAt(j)) {
-                return false; // If mismatch, it's not a palindrome
+         Scanner sc = new Scanner(System.in);
+	 System.out.println("enter the text");
+	 String word = sc.nextLine();
+	 char[] word1 = word.toCharArray();
+	int start = 0;
+	int end = word1.length-1;
+        while(start<end){
+           
+            if(word1[start] != word1[end]){
+                System.out.println("not palindrom");
+               return ; 
             }
-            i++;
-            j--;
+           
+           start++;
+           end --;
         }
-        
-        return true; // If no mismatches, it's a palindrome
+       System.out.println(" palindrom"); 
     }
 }
