c3399d0b3bc66f3286f37788d5cee97891caae59
aditihumne <147162686+aditihumne@users.noreply.github.com>
2025-08-18 22:49:31 +0530

    Add files via upload

---
+++ Strings/Concatenation of Two Strings.java
@@ -0,0 +1,19 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+	String str ="hello";
+	String str2 ="world";
+	String result = str+str2;
+	System.out.println(result);
+
+	    
+	}
+}
