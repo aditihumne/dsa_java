79435b6bae5e823ccf72cc6451ce77e5353366f5
aditihumne <147162686+aditihumne@users.noreply.github.com>
2025-10-07 15:42:14 +0530

    Update Stock Buy and Sell – One Transaction.java

--- Arrays/Stock Buy and Sell – One Transaction.java
+++ Arrays/Stock Buy and Sell – One Transaction.java
@@ -13,7 +13,7 @@ public class Main
 		int max=0;
 		for(int i=0;i<arr.length-1 ;i++){
 		    for(int j =i+1;j<arr.length;j++){
-		        max= Math.max(max,arr[j]-arr[i]);
+		        max= Math.max(max,arr[i]-arr[j]);
 		    }
 		}
 		System.out.println(max);
